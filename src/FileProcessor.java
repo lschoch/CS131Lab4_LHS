c95218f120605bffd6ad309354813fc5e37bdf9b
lschoch@gmail.com <lschoch@gmail.com>
2021-11-23 20:34:10 -0500

    Now catches FileNotFoundException instead of IOException. Javadocs and comments.

--- src/FileProcessor.java
+++ src/FileProcessor.java
@@ -1,62 +1,66 @@
 import java.util.Scanner;
 import java.util.ArrayList;
-import java.io.*;
+import java.io.FileNotFoundException;
+import java.io.File;
 
+/**
+ * Class to print text files that have one string per line. Catches and 
+ * handles FileNotFoundException and StringTooLongException.
+ * 
+ * @author lschoch
+ * @version 1.0 CS-131-ON Lab4
+ */
 public class FileProcessor {
-
-	private String fileName;
-	private int stringLength;
-	private ArrayList<String> stringList;
-	private Scanner input;
 	
+	private String fileName; // The name of the file to be processed.
+	private int stringLength; // The maximum length allowed for the single 
+			// string on each line of the file.
+	private ArrayList<String> stringList; // An ArrayList object (array)
+	private Scanner input; // Scanner object for reading the file line by line.
+	
+	/**
+	 * Constructor to create a FileProcessor object specifying file name and 
+	 * maximum allowed length of the single string on each line.
+	 * 
+	 * @param fileName the name of the file to be processed
+	 * @param stringLength the maximum length allowed for each string
+	 */
 	public FileProcessor(String fileName, int stringLength) {
+		// Minimum string length is 5
 		if (stringLength < 5) {
 			stringLength = 5;
 		}
 		setStringLength(stringLength);
 		setFileName(fileName);
 			
-	}// end empty-argument constructor
+	}// end constructor
 	
-	public String getFileName() {
-		return fileName;
-	}// end getFileName
-
-	public void setFileName(String fileName) {
-		this.fileName = fileName;
-	}
-
-	public int getStringLength() {
-		return stringLength;
-	}// end get StringLength
-
-	public void setStringLength(int stringLength) {
-		if (stringLength < 5) {
-			stringLength = 5;
-		}
-		this.stringLength = stringLength;
-	}
-
-	public int getArrayListSize() {
-		return stringList.size();
-	}//end getArrayListSize
-	
+	/**
+	 * Method to read and print each line of the file specified by fileName,
+	 * includes catching and handling FileNotFoundException and 
+	 * StringTooLongException. 
+	 */
 	public void processFile() {
+		// Instantiate a StringTooLongException
 		StringTooLongException stlException = new StringTooLongException("String is too long!");
 		String line = "";
 		
+		// Check for file not found, can't proceed without a file to read.
 		try {
 			File file = new File(fileName);
 			input = new Scanner(file);
 		}
-		catch(IOException io) {
-			System.out.println(io.getMessage());
+		catch(FileNotFoundException fnf) {
+			System.out.println(fnf.getMessage());
 			return;
 		}
 		
+		// Step through the file line by line
 		while (input.hasNextLine()) {	
 			try {
 				line = input.nextLine();
+				// Check line length, throw exception if greater than 
+				// stringLength, otherwise print line. 
 				if (line.length() > stringLength) {
 					throw stlException;
 				}
@@ -68,5 +72,59 @@ public class FileProcessor {
 		}
 		input.close();
 	}//end processFile
+	
+	/**
+	 * Method to get value of the fileName parameter.
+	 * 
+	 * @return the current value of fileName
+	 */
+	public String getFileName() {
+		return fileName;
+		
+	}// end getFileName
+	
+	/**
+	 * Method to set a new value for the fileName parameter.
+	 * 
+	 * @param fileName the new value for fileName
+	 */
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+		
+	}// end setFileName
+	
+	/**
+	 * Method to get the value of the stringLength parameter.
+	 * 
+	 * @return the current value of stringLength
+	 */
+	public int getStringLength() {
+		return stringLength;
+		
+	}// end get StringLength
+	
+	/**
+	 * Method to set a new value for the stringLength parameter.
+	 * 
+	 * @param stringLength the new value for stringLength
+	 */
+	public void setStringLength(int stringLength) {
+		// Minimum string length is 5
+		if (stringLength < 5) {
+			stringLength = 5;
+		}
+		this.stringLength = stringLength;
+		
+	}// end setStringLength
+	
+	/**
+	 * Method to get the current size of stringList.
+	 * 
+	 * @return the current size of stringList
+	 */
+	public int getArrayListSize() {
+		return stringList.size();
+		
+	}//end getArrayListSize
 	
 }//end class
